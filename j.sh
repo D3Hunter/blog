8f6ab36629a6e791724a4abef0bc7c10b87146c9
D3Hunter <jujj603@gmail.com>
2014-11-11 17:18:53 +0800

    Modify

--- j.sh
+++ j.sh
@@ -1,5 +1,3 @@
-# set environment variables for all user
-# put in /etc/profile.d/
 export JAVA_HOME=/usr/lib/jvm/default-java
 export JRE_HOME=${JAVA_HOME}/jre
 export CATALINA_HOME=/usr/share/tomcat7
@@ -10,4 +8,6 @@ export PATH=${JAVA_HOME}/bin:/usr/local/nginx/sbin:$PATH
 alias ll='ls -lrt'
 alias l='ls -l'
 alias emacs='emacs -nw'
+alias grep='grep --exclude-dir=\.?* --exclude=\.?*'
 TERM=xterm-256color
+#force_color_prompt=yes
